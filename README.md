783097edb120078fe4533f46dfe37996a03ad6a1
Yunhyeok Lee <yunhyeoklee@Yunhyeoks-MacBook-Pro.local>
2017-02-21 20:16:35 -0600

    Add Download Webm File into Local

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Project_dandelion
 By Kevin and Yunhyeok 
 
-Feb 20 기록.. 
+Feb 21 기록
 1. Firebase랑 연동
 
 - Extension에서 쓰는 Permission을 끌어다 써본 결과..
@@ -16,21 +16,14 @@ Extension에서는 저 Permission만 넣고 파이어베이스 일반 웹에서 
 2)  window.html:1 window.localStorage is not available in packaged apps. Use chrome.storage.local instead.
 
 
-
 2. Kiosk 사용 - 케시에 저장 안해도 되고, 편하다고 함..
 관련 링크 1. - https://developer.chrome.com/apps/fileSystem#method-requestFileSystem
 관련 링크 2. - https://developer.chrome.com/apps/manifest/kiosk_enabled
 
 
-
-
 3. RequestFileSystem - 크롬 OS에서 작동 하는 것 같음 (맥, Window에서 작동 X)
 
-아래 부분이 중요한 부분이야!
 
-requestFileSystem
-
-chrome.fileSystem.requestFileSystem(object options, function callback)
-Since Chrome 44.
-
-Requests access to a file system for a volume represented by options.volumeId. If options.writable is set to true, then the file system will be writable. Otherwise, it will be read-only. The writable option requires the "fileSystem": {"write"} permission in the manifest. Available to kiosk apps running in kiosk session only. For manual-launch kiosk mode, a confirmation dialog will be shown on top of the active app window. In case of an error, fileSystem will be undefined, and chrome.runtime.lastError will be set.
+4. 해야 할 것..
+ 4-1)Blob 객체 파일 받아서.. 파일로 변환 하고 그거를 해당 디렉터리에 저장 - 완료 
+ 4-2)삭제도 하기 
--- window.js
+++ window.js
@@ -5,38 +5,68 @@ var sv_file = document.querySelector('#save_file');
 var output = document.querySelector('output');
 var importData = document.querySelector('#importData'); // 뭐 열었는지 보여주는거
 var import_data = document.querySelector('#import_data'); // 버튼 
-var blobList = [];
-var entry;
+var write_file = document.querySelector("#write_file"); //Blob을 쓰는걸 해보기 위해서 만든 버튼! 
 
+var blobList = []; //여기다가 여러게가 들어가!!
+var entry;
 function errorHandler(e) {
   console.error(e);
 }
+// 우선 Load_Video로 blobList에 있는 걸 우선 받아오고, 그 다음 이거 실행하면 내가 원하는 곳에 다운이 가능!
+//실행 순서.. 1. load_video로 blobList에 넣는다 2. write File 버튼 눌러서 디렉토리 지정해서 파일 저장한다.
+write_file.addEventListener('click', function() {
+  var accepts = {
+    type: 'openDirectory',
+    'accepts':[{ mimeTypes:['video/*']}, // object (curly bracket하는거)
+    {'description':'This is example'}, // string (bracket만 하는거)
+    {'extensions' :['webm','mp4']}] 
+  }
+  //console.log(blobList[0]);
+  chrome.fileSystem.chooseEntry(accepts, function(theEntry) {
+    chrome.fileSystem.getWritableEntry(theEntry, function(theEntry2) {
+      theEntry2.getFile('Test2.webm', {create:true}, function(theEntry3){
+        theEntry3.createWriter(function(writer) {
+          writer.write(blobList[0]);
+        })
+      })
+    })
+  })
+})
 
 //load video to arraybuffer. Get ready to play Videos
 load_video.addEventListener('click', function() {
+  var accepts = {
+    type: 'saveFile',
+    'accepts':[{ mimeTypes:['video/*']}, // object (curly bracket하는거)
+    {'description':'This is example'}, // string (bracket만 하는거)
+    {'extensions' :['webm','mp4']}] 
+  }
   var vid2 = "http://www.ioncannon.net/examples/vp8-webm/big_buck_bunny_480p.webm"
   var vid = "http://techslides.com/demos/sample-videos/small.webm"
+  var blob;
   var request = new XMLHttpRequest();
   request.responseType = "arraybuffer"
   request.onload = function(e) {
-    var blob = new Blob([request.response], {type: "video/webm"});
-    console.log(blob)
+    blob = new Blob([request.response], {type: "video/webm"});
+   
     blobList.push(blob)      
   }
-  request.open("GET", vid, true)
-  request.send()
+
   
   var request2 = new XMLHttpRequest()
   request2.responseType = "arraybuffer"
   request2.onload = function(e) {
-    var blob = new Blob([request2.response], {type: "video/webm"});
-    console.log(blob)
+    blob = new Blob([request2.response], {type: "video/webm"});
+    console.log(blob);
     blobList.push(blob)
-  }
+  }  
   request2.open("GET", vid2, true)
   request2.send()
-})
+  request.open("GET", vid, true)
+  request.send()
 
+})
+  
 //play videos which are load at load_video function at Chrome App 
 playVideo.addEventListener('click', function() {
   // console.log(blobList)
@@ -51,6 +81,8 @@ playVideo.addEventListener('click', function() {
 
 //https://developer.mozilla.org/en-US/docs/Web/API/File <- 이걸로 보면됨
 
+
+//이거는 일단 거의 필요가 없는건데.. FileEntry랑.. 개인 Path를 받아 오는것 까지 공부하는거..
 ch_file.addEventListener('click', function(e) {
   var accepts = {
     type: 'openDirectory',
@@ -60,61 +92,35 @@ ch_file.addEventListener('click', function(e) {
   }
 
   chrome.fileSystem.chooseEntry(accepts, function(theEntry) {
-    var dirReader = theEntry.createReader();
-    console.log(theEntry+" 1");
-    if(!theEntry) {
-      output.textContent = 'No file selected.';
-      return;
-    }
-    console.log(dirReader+" 2");  
-  
-  chrome.fileSystem.isWritableEntry(theEntry, function(reserved) {
-    console.log(reserved+" 3");
-  })
-  
-  var entries = [];
-  dirReader.readEntries(function(results) {
-    //console.log(results);
-    results.forEach(function(item) {
-      entries.push(item.fullPath);
+      var dirReader = theEntry.createReader();
+      console.log(theEntry+" 1");
+      if(!theEntry) {
+        output.textContent = 'No file selected.';
+        return;
+      }
+      console.log(dirReader+" 2");  
+    
+    chrome.fileSystem.isWritableEntry(theEntry, function(reserved) {
+      console.log(reserved+" 3");
+    })
+    
+    var entries = [];
+    dirReader.readEntries(function(results) {
+      //console.log(results);
+      results.forEach(function(item) {
+        entries.push(item.fullPath);
+      })
+
+      var find = /Korea/i // 여기다가 찾고 싶은 것의 이름을 넣으면 되! //하나의 파일만 찾아 내고 싶을 때,
+      var filter = entries.filter(function(item, index, array) {
+            return !!~item.search(find);
+        });
+        console.log(filter+" 4");
     })
 
-     var find = "Korea" // 여기다가 찾고 싶은 것의 이름을 넣으면 되! //하나의 파일만 찾아 내고 싶을 때,
-     var filter = entries.filter(function(item, index, array) {
-          return !!~item.search(find);
-      });
-      console.log(filter+" 4");
+    chrome.fileSystem.getDisplayPath(theEntry, function(reserved2) { //File Path 어디인지 알아 낼 때
+      console.log(reserved2+" 5");
+    })
   })
-
-  chrome.fileSystem.getDisplayPath(theEntry, function(reserved2) { //File Path 어디인지 알아 낼 때
-    console.log(reserved2+" 5");
-  })
-
 })
-
-    // chrome.fileSystem.restoreEntry("test", function (reserved3) {
-    //   console.log(reserved3);
-    // }) // 이런거에서 id는 어디에서 가져 오는거지?
-
-})
-
-
-// var defaultDatabase = firebase.database();
-// firebase.database.enableLogging(true);
-
-// Firebase.INTERNAL.forceWebSockets();
-// var database = firebase.database();
-
-//   // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyAXO8kgV-tgvtRwyUmF2YUoH2qQUMLhJMc",
-//     authDomain: "project-dandelion.firebaseapp.com",
-//     databaseURL: "https://project-dandelion.firebaseio.com",
-//     storageBucket: "project-dandelion.appspot.com",
-//     messagingSenderId: "551863919971"
-//   };
-//   firebase.initializeApp(config);
-
-
-// var rootRef = firebase.database().ref();
 
